759d7615bf6bb26a8520d03c59a962c8a39b82cf
Κωνσταντινος Ματτας <109415079+Malakotadin@users.noreply.github.com>
2026-06-03 23:03:33 +0300

    Add files via upload

---
+++ fast_package2/map_wencode.py
@@ -0,0 +1,160 @@
+import subprocess
+from mpmath import mp,mpf,workdps,workprec,autoprec
+import time,sys
+import pickle
+from mapped_encode import encoder
+from parallel_decode import decoder
+import threading
+from p_tqdm import p_map
+image_path='gimp.bmp'
+#bytes.hex
+f= open(image_path,'rb')
+bint = f.read()
+f.close()
+mp.dps=1030#γαμω γαμω γαμω γαμω εχασα 20 χρονια απο το προσδοκιμω ζωης μου και απέκτησα άλλο ένα φαλακρό σημείο στο κεφάλι μου για αυτή την γραμμή κώδικα
+demo_string=bint.hex()
+splicing_counter=0
+arg=demo_string[:256]
+noumero=0
+strnoumero=str(noumero)
+print(arg)
+lista_kopsimatos=[]
+
+#exit()
+infinite_mhden=mp.mpf(0.0)
+tag_ls=len(demo_string)*[infinite_mhden]
+ls_pososta_ls=[0]*len(demo_string)
+ls_xarakthres_ls=[0]*len(demo_string)
+#breakpoint()
+arithmos_bit=256
+start=time.time()
+while(True):
+    if splicing_counter+arithmos_bit>len(demo_string):
+        arg=demo_string[splicing_counter:len(demo_string)]
+        temp=arg
+        lista_kopsimatos.append(arg)
+        diafora=len(demo_string)-splicing_counter
+        print(diafora)
+      #p  breakpoint()
+        #timoula,lista_arhikon_pososton,lista_monadikon_xarakthron=subprocess.check_output(['python','encode_to_files3.py',arg,strnoumero])
+      #  encode=threading.Thread(None,encoder(arg,tag_ls,ls_pososta_ls,ls_xarakthres_ls,noumero))
+      #  timoula,lista_arhikon_pososton,lista_monadikon_xarakthron=threading.Thread(target=encoder,args=arg)
+        
+        
+        break
+    else: 
+        arg=demo_string[splicing_counter:splicing_counter+arithmos_bit]
+        lista_kopsimatos.append(arg)
+      #  saving_list=subprocess.check_output(['python','encode_to_files3.py',arg,strnoumero])
+        #timoula=subprocess.check_output(['python','encode_to_files3.py',arg,strnoumero])
+       # breakpoint()
+        #timoula,lista_arhikon_pososton,lista_monadikon_xarakthron=encoder(arg)
+        #saving_list
+       # print(saving_list)
+      #  print(lista_arhikon_pososton)
+     #  timoula=timoula.decode()
+       # print(timoula)
+       # print(timoula)
+       # encode=threading.Thread(None,encoder(arg,tag_ls,ls_pososta_ls,ls_xarakthres_ls,noumero))
+       # ls_xarakthres_ls[noumero]=lista_monadikon_xarakthron
+       ## ls_pososta_ls[noumero]=lista_arhikon_pososton
+        #tag_ls[noumero]=timoula
+    #noumero+=1
+    #strnoumero=str(noumero)
+    splicing_counter+=arithmos_bit
+    print(splicing_counter)
+
+
+    #if i==len(lista_kopsimatos):
+       # tagl_ls,ls_pososta_ls,ls_xarakthres_ls=list(map(encoder(diafora)))
+def apothikeutis0(item):
+    if item[0]!=infinite_mhden and item[0]!=0:
+        return item[0]#εδω περα ισως μπορω να βγαζω να αφερει τα μηδενικα
+    else:
+      print("i happened0")
+def apothikeutis1(item):
+    if item[1]!=infinite_mhden and item[1]!=0:
+        return item[1]#εδω περα ισως μπορω να βγαζω να αφερει τα μηδενικα
+    else:
+      print("i happened1")
+def apothikeutis2(item):
+    if item[2]!=infinite_mhden and item[2]!=0:
+        return item[2]#εδω περα ισως μπορω να βγαζω να αφερει τα μηδενικα
+    else:
+      print("i happened2")
+    
+
+lista_apothikeusis=p_map(encoder,lista_kopsimatos)
+
+#breakpoint()
+tag_ls=p_map(apothikeutis0,lista_apothikeusis)
+#breakpoint()
+ls_pososta_ls=p_map(apothikeutis1,lista_apothikeusis)
+ls_xarakthres_ls=p_map(apothikeutis2,lista_apothikeusis)
+#breakpoint()
+#exit()
+#apothikeutiki_lista=[arithmos_bit,tag_ls,ls_pososta_ls,ls_xarakthres_ls,noumero,diafora]
+
+def cleaner(lista):
+  for i in range(0,len(lista)):
+    try:
+      if lista[i]==infinite_mhden:
+        lista.pop(i)
+        print("i did something")
+    except:
+      pass
+
+  return lista
+noumero=len(tag_ls)
+def selfer(value):
+  #print(mp.dps)
+  
+  value=value[0]
+  value=autoprec(value)
+  return value
+#tag_ls=cleaner(tag_ls)
+#ls_pososta_ls=cleaner(ls_pososta_ls)
+#ls_xarakthres_ls=cleaner(ls_xarakthres_ls)
+#clean=threading.Thread(None,cleaner(tag_ls))#η  μαλακια ειναι οτι τα σωζο σε πολυ μεγαλο precisoin και αυτο μου τρώει τοσο χώρο, what do i do  ? 
+#clean=threading.Thread(None,cleaner(ls_pososta_ls))
+#clean=threading.Thread(None,cleaner(ls_xarakthres_ls))
+breakpoint()
+#tag_ls=p_map(autoprec,lista_apothikeusis)
+#breakpoint()
+#ls_pososta_ls=p_map(autoprec,lista_apothikeusis)
+#ls_xarakthres_ls=p_map(autoprec,lista_apothikeusis)
+file_to_send=open("tag_ls",'wb')
+pickle.dump(tag_ls,file_to_send)
+file_to_send.close()
+print("δεν μπηκαα")
+with workdps(500):
+  print("μπικααααα)")
+  print(mp.dps)
+  tag_ls=p_map(selfer,lista_apothikeusis)
+  #breakpoint()
+  #ls_pososta_ls=p_map(selfer,lista_apothikeusis)
+  #ls_xarakthres_ls=p_map(selfer,lista_apothikeusis)
+  print(mp.dps)
+  breakpoint()
+
+  file_to_send=open("tag_ls",'wb')
+  pickle.dump(tag_ls,file_to_send)
+  file_to_send.close()
+  exit()
+  file_to_send=open("arithmos_bit",'wb')
+  pickle.dump(arithmos_bit,file_to_send)
+  file_to_send.close()
+  file_to_send=open("ls_pososta_ls",'wb')
+  pickle.dump(ls_pososta_ls,file_to_send)
+  file_to_send.close()
+  file_to_send=open("ls_xarakthres_ls",'wb')
+  pickle.dump(ls_xarakthres_ls,file_to_send)
+  file_to_send.close()
+  file_to_send=open("noumero",'wb')
+  pickle.dump(noumero,file_to_send)
+  file_to_send.close()
+  file_to_send=open("diafora",'wb')
+  pickle.dump(diafora,file_to_send)
+  file_to_send.close()
+
+#breakpoint()
---
+++ fast_package2/mapped_encode.py
@@ -0,0 +1,149 @@
+from mpmath import mp,mpf
+import time,sys
+import pickle
+
+
+
+
+#print(bin)
+flag=0
+
+
+#mp.dps=1
+#hex_bin=bin.hex()#μετατροπή σε hex
+#print(type(hex_bin))
+#arithmos_bit=len(hex_bin)
+def encoder(demo_string):
+    mp.dps=1030
+    #breakpoint()
+   # demo_string=sys.argv[1]
+    #id=int(sys.argv[2])
+    arithmos_bit=len(demo_string)
+    def string_yparhei(dictionary_xarakthron_kai_pososton,char):
+        for key in dictionary_xarakthron_kai_pososton:
+            if char==key:
+                dictionary_xarakthron_kai_pososton[char]=dictionary_xarakthron_kai_pososton[char] +1
+                return dictionary_xarakthron_kai_pososton
+        dictionary_xarakthron_kai_pososton[char]=1
+        return dictionary_xarakthron_kai_pososton
+
+    def diavasma_string(string):
+        dictionary_xarakthron_kai_pososton={}
+        
+        
+        for char in string:
+            
+            dictionary_xarakthron_kai_pososton=string_yparhei(dictionary_xarakthron_kai_pososton,char)
+            
+        return dictionary_xarakthron_kai_pososton 
+        
+
+
+    #νιωθω αυτη η υλοποίηση είναι πολύ καλή 
+
+    dictionary_xarakthron_kai_pososton=diavasma_string(demo_string)
+    lista_monadikon_xarakthron=[]
+    for key in dictionary_xarakthron_kai_pososton:
+        lista_monadikon_xarakthron.append(key)
+    temp=0
+    #print(lista_monadikon_xarakthron)
+    #breakpoint()
+    lista_arhikon_pososton=[]
+    lista_olikon_poston=[]
+    metritis_monadikon_xarakthron=0
+    for key in dictionary_xarakthron_kai_pososton:
+        lista_arhikon_pososton.append(dictionary_xarakthron_kai_pososton[key])
+        metritis_monadikon_xarakthron=metritis_monadikon_xarakthron+1
+        dictionary_xarakthron_kai_pososton[key]=dictionary_xarakthron_kai_pososton[key]/arithmos_bit +temp
+        lista_olikon_poston.append(dictionary_xarakthron_kai_pososton[key])
+        temp= dictionary_xarakthron_kai_pososton[key]
+        
+    #print(lista_arhikon_pososton)
+    #breakpoint()
+    #print(dictionary_xarakthron_kai_pososton)
+    #breakpoint()
+    start_range=0
+    end_range=1
+    start_range=mp.mpf(start_range)
+    end_range=mp.mpf(end_range)
+    #print(lista_arhikon_pososton)
+
+
+    lista_arhikon_pososton=[timi/arithmos_bit for timi in lista_arhikon_pososton]
+    #print(lista_olikon_poston)
+    #breakpoint()
+
+    def olika_pososta(kato_orio,ano_orio,lista_pithanotiton):
+        oliko_pososto=[kato_orio]
+        diafora=ano_orio-kato_orio
+        kato_orio_xarakthra=kato_orio
+        for pithanotita in lista_pithanotiton:
+            ano_orio_xarakthra=kato_orio_xarakthra+(diafora*pithanotita)
+            oliko_pososto.append(ano_orio_xarakthra)
+            kato_orio_xarakthra=ano_orio_xarakthra
+        return oliko_pososto
+
+    def apostasi_tou_kleidou(lista_olikon_poston,lista_monadikon_xarakthron):
+        interval={}
+        i=0
+        j=0 
+        while i <len(lista_olikon_poston)-1:
+            #print(len(lista_olikon_poston)-i)
+            key = lista_monadikon_xarakthron[i]
+            kato_orio=lista_olikon_poston[i]#εδω περα οριζονται τα ορια 
+            ano_orio=lista_olikon_poston[i+1]
+            interval[key]=[kato_orio,ano_orio]
+            i+=1
+            j+=1
+        return interval
+
+    def encoderv2(lista_arhikon_pososton,lista_monadikon_xarakthron,demo_string):
+        start=0
+        start=mp.mpf(start)
+        end=1
+        end=mp.mpf(end)
+        oliki_lista_pososton=olika_pososta(start,end,lista_arhikon_pososton)
+        dictionary_diastimaton=apostasi_tou_kleidou(oliki_lista_pososton,lista_monadikon_xarakthron)
+    # print("η λιστα ολικων ποσοστων ειναι η ",oliki_lista_pososton)
+        timoula=0
+        timoula=mp.mpf(timoula)
+        for character in demo_string:
+            char_diastima=dictionary_diastimaton.get(character)
+            kato_orio_xarakthra=char_diastima[0]
+            ano_orio_xarakthra=char_diastima[1]
+            timoula=(kato_orio_xarakthra+ano_orio_xarakthra)/2.0
+            oliki_lista_pososton=olika_pososta(kato_orio_xarakthra,ano_orio_xarakthra,lista_arhikon_pososton)
+            dictionary_diastimaton=apostasi_tou_kleidou(oliki_lista_pososton,lista_monadikon_xarakthron)
+
+        return timoula
+
+    timoula=encoderv2(lista_arhikon_pososton,lista_monadikon_xarakthron,demo_string)#ωραια αυτο επιστρεφει επιτυχως τιμη
+
+    #ls_xarakthres_ls[noumero]=lista_monadikon_xarakthron
+    #ls_pososta_ls[noumero]=lista_arhikon_pososton
+    #tag_ls[noumero]=timoula
+    lista_epistrogis=[]
+    lista_epistrogis.append(timoula)
+    lista_epistrogis.append(lista_arhikon_pososton)
+    lista_epistrogis.append(lista_monadikon_xarakthron)
+    return lista_epistrogis
+    #return timoula,lista_arhikon_pososton,lista_monadikon_xarakthron
+    #print(demo_string)
+   # print(timoula)
+    #print(lista_arhikon_pososton)
+    #print(lista_monadikon_xarakthron)
+    #return timoula
+    #breakpoint()
+    #exit()
+    #with open(f"timoula{id}","wb") as file:
+    #   pickle.dump(timoula,file)
+    #filename=f"arhiki_lista_sinolon{id}"
+    ##   pickle.dump(lista_arhikon_pososton,file)
+
+    #with open(f"xarakthres{id}","wb") as file:
+    #   pickle.dump(lista_monadikon_xarakthron,file)
+    #final_output="".join(decoderv2(timoula,arithmos_bit,lista_arhikon_pososton))
+    #print(arithmos_bit)
+    #print(demo_string)
+    #print(final_output)
+    #print(demo_string==final_output)
