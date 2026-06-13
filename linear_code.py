3e09651d173770354c861c168f0965f0ae43dcb5
micmanas2 <manosforinsta@gmail.com>
2026-06-13 20:10:55 +0300

    Create linear_code.py

---
+++ linear_code.py
@@ -0,0 +1,87 @@
+from sage.all import *
+import random
+
+image = ""
+
+while len(image) % 4 != 0:     # padding
+          image += "0"
+
+chunks = [image[i:i+4] for i in range(0, len(image), 4)]   # split into chunks of 4
+          
+M = GF(2)
+
+# 16 διαφορετικά μηνύματα(2^4)
+# 128 διαφορετικοί vectors(2^7)
+
+G = matrix(M, [                # generator matrix απο το βιβλιιο (7,4)
+          [1,0,0,0,1,1,1],
+          [0,1,0,0,1,1,0],
+          [0,0,1,0,1,0,1],
+          [0,0,0,1,0,1,1]
+          ])           
+
+H =  matrix(M, [              # πίνακας ελέγχου από το βιβλίο (7,3)
+            [1,1,1,0,1,0,0],
+            [1,1,0,1,0,1,0],
+            [1,0,1,1,0,0,1]
+            ])
+
+# Κάθε vector() ή tuple() αλλάζει τον τύπο τις μεταβλητής όπως χρειάζεται για να μπορέσουν να γίνουν οι πράξεις
+
+def encode(c):                # πολλαπλασιαζμός μηνύματος(c) με generator matrix
+  return vector(M, c) * G
+
+def error_syndrome(r):        # πολλαπλασιαμός με πίνακα ελέγχου. r = c + e(error)
+   return H * vector(M, r)
+
+# 128/16 = 8 σύμπλοκα
+
+def build_error_syndrome_table():
+  table = {}
+  zero = vector(M, [0,0,0,0,0,0,0])
+  table[tuple(error_syndrome(zero))] = zero # no error
+  for i in range(7):
+    e = [0,0,0,0,0,0,0]
+    e[i] =  1
+    e = vector(Μ, e)
+    table[tuple(error_syndrome(e))] = e
+  return table    
+
+error_syndrome_table = build_error_syndrome_table()
+
+
+def decode(r):
+  r = vector(M, r)
+  s = tuple(error_syndrome(r))
+  if all (v == 0 for v in s):          # αν = 000 σημάινει οτι δεν έχει σφάλματα
+    print("No errors")
+    corrected = r
+  elif s in error_syndrome_table:
+    corrected = r - error_syndrome_table[s]  # XOR
+  else:
+    print("More than 1 errors. Can't be corrected")
+    return None
+
+  message = list(corrected)[:4]          # Παίρνουμε τα πρώτα 4 bits που είναι = αρχικό μήνυμα
+  print(f"Decoded message: {message}")
+  return message
+
+
+
+def add_errors(codeword, error_percent):
+
+    codeword = list(codeword)
+    n = len(codeword)    # πάντα 7 
+    num_errors =  int(n * error_percent))  # στρογγυλοποίηση προς τα κάτω -  θα βάλει 0 ως 7 μοναδικά σφάλματα(error_percent 0-1)
+    
+    error_positions = random.sample(range(n), min(num_errors, n)) # βρίσκει τις θέσεις στις οποίες θα βάλει τα σφάλματα
+    
+    for pos in error_positions:
+         codeword[pos] = int(codeword[pos]) ^ 1   # στις θέσεις που υπολόγισε αλλάζει τα bits
+         return codeword
+
+for i, chunk in enumerate(chunks):
+    bits = [tuple(b) for b in chunk]
+    codeword = encode(bits)
+    received = add_errors(codeword, error_percent=0.10)
+    decode(received)
